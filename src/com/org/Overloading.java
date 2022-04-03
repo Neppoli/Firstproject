6437cf9da53b037f20634b1567d3dd3350ce0dce
Neppoli <tneppoli@gmail.com>
2022-04-03 08:24:32 +0530

    program

---
+++ src/com/org/Overloading.java
@@ -0,0 +1,14 @@
+package com.org;
+
+public class Overloading {
+	private void complany(String name,int id,long phone) {
+		// TODO Auto-generated method stub
+System.out.println("Empname:"+name+"\n"+"Emp id:"+id+"\n"+"Emp phone"+phone);
+	}
+	public static void main(String[] args) {
+		Overloading c= new Overloading();
+		c.complany("moni", 234, 8883846512l);
+		System.out.println("******************");
+		c.complany("rocky", 235, 8248235760l);
+	}
+}
